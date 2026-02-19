5aa96e7a6908c7b9a9e0b1f80d8e7932c14a35cf
Bikram Das <sd475807@gmail.com>
2026-02-19 23:46:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (24.49%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.c
@@ -0,0 +1,23 @@
+int searchInsert(int* nums, int numsSize, int target) {
+  int low = 0;
+    int high = numsSize-1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // Check if x is present at mid
+        if (nums[mid] == target)
+            return mid;
+
+        // If x greater, ignore left half
+        if (nums[mid] < target)
+            low = mid + 1;
+
+        // If x is smaller, ignore right half
+        else
+            high = mid - 1;
+    }
+
+    // If we reach here, then element was not present
+    return low;
+}
+    
