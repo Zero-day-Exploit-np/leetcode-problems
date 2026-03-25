a043e66430cbcebfa7a62f586e8f73483350f107
Bikram Das <sd475807@gmail.com>
2026-03-25 20:56:37 +0530

    Time: 3 ms (6.29%) | Memory: 10.2 MB (5.20%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.c
+++ 20-valid-parentheses/valid-parentheses.c
@@ -1,13 +1,3 @@
-int ismatching(char open, char close) {
-    if (open == '[' && close == ']')
-        return 1;
-    if (open == '{' && close == '}')
-        return 1;
-    if (open == '(' && close == ')')
-        return 1;
-    return 0;
-}
-
 struct Node {
     char data;
     struct Node* next;
@@ -18,16 +8,24 @@ int isEmpty(struct Node* ptr) {
     }
     return 0;
 }
-
 int isFull() {
-    struct Node* a = (struct Node*)malloc(sizeof(struct Node));
-    if (a == NULL) {
+    struct Node* ptr = (struct Node*)malloc(sizeof(struct Node));
+    if (ptr == NULL) {
         return 1;
     }
-    free(a);
+    free(ptr);
     return 0;
 }
 
+struct Node* push(struct Node* ptr, char info) {
+    if (isFull()) {
+        return 0;
+    }
+    struct Node* a = (struct Node*)malloc(sizeof(struct Node));
+    a->data = info;
+    a->next = ptr;
+    return a;
+}
 struct Node* pop(struct Node* ptr) {
     if (isEmpty(ptr)) {
         return NULL;
@@ -38,38 +36,36 @@ struct Node* pop(struct Node* ptr) {
     return ptr;
 }
 
-struct Node* push(struct Node* ptr, char data) {
-    if (isFull()) {
-
-        return 0;
-    }
-    struct Node* a = (struct Node*)malloc(sizeof(struct Node));
-    a->data = data;
-    a->next = ptr;
-    return a;
+int ismatching(char open, char close) {
+    if (open == '(' && close == ')')
+        return 1;
+    if (open == '{' && close == '}')
+        return 1;
+    if (open == '[' && close == ']')
+        return 1;
+    return 0;
 }
 
-
 bool isValid(char* s) {
-struct Node* head = NULL;
+    struct Node* head = NULL;
+
     for (int i = 0; s[i] != '\0'; i++) {
         if (s[i] == '[' || s[i] == '{' || s[i] == '(') {
             head = push(head, s[i]);
         } else if (s[i] == ']' || s[i] == '}' || s[i] == ')') {
             if (isEmpty(head)) {
-                return 0;
+                return FALSE;
             }
             char a = head->data;
             head = pop(head);
             if (!ismatching(a, s[i])) {
-                return 0;
+                return FALSE;
             }
         }
     }
-
     if (head == NULL) {
-        return 1;
+        return TRUE;
     } else {
-        return 0;
+        return FALSE;
     }
 }
