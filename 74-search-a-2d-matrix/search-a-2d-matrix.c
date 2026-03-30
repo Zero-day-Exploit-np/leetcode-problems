9b9da2c321cf2b5fe7d8f6ce788471f6d13a2b8b
Bikram Das <sd475807@gmail.com>
2026-03-30 19:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (25.73%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.c
@@ -0,0 +1,26 @@
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+     int rows = matrixSize;
+    int cols = matrixColSize[0];
+       int left = 0;
+    int right = rows * cols - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        int midRow = mid / cols;
+        int midCol = mid % cols;
+        int midValue = matrix[midRow][midCol];
+
+        if (midValue == target) {
+            return true;
+        }
+        else if (midValue < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+}
